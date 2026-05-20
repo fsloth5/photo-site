a0bb5aa07bbfeef939dc0b0668a4bdccface2cdf
fsl0th5 <gcelisramirez@student.cccd.edu>
2026-05-20 00:22:09 -0700

    Begin site responsiveness "Welcome"

--- static/index.js
+++ static/index.js
@@ -57,17 +57,53 @@ const removeAndAdd = (originalAnimation, newAnimation) => {
 	};
 };
 
-const onMobile = () => {
-	if (localStorage.getItem("isOnMobile") === null) {
-		localStorage.setItem("isOnMobile", true);
-	}
+const onHomeInMobilePhones = () => {
+	const contentContainer = document.getElementById("home-page-content");
+	contentContainer.classList.replace("hbox", "vbox");
 
+	contentContainer.firstElementChild.lastElementChild.classList.replace(
+		"align-end",
+		"center-self",
+	);
+
+	const headerContainer = contentContainer.firstElementChild.firstElementChild;
+
+	headerContainer.classList.replace("centered-cross", "centered-main");
+	headerContainer.classList.remove("align-start");
+
+	const mainImage = document.getElementById("welcome-img");
+	mainImage.src = "/resources/main_mobile.webp";
+	mainImage.classList.remove("w900px");
+	mainImage.classList.add("w100");
+
+	const quotesContainer = document.getElementById("quotes-container");
+	mainImage.remove();
+	const textToSplit = quotesContainer.lastElementChild;
+	quotesContainer.appendChild(mainImage);
+	textToSplit.innerText = '"And to do that, I only ask one thing...';
+	const remainingText = document.createElement("p");
+	remainingText.innerText =
+		'let me into your world even if for just a brief moment."';
+	remainingText.classList.add(...textToSplit.classList);
+	remainingText.classList.replace("t-align-start", "t-align-center");
+	quotesContainer.appendChild(remainingText);
+
+	const spacer = document.createElement("div");
+	spacer.classList.add("flex-one", "margin-sm");
+	contentContainer.appendChild(spacer);
+};
+
+const onHome = () => {
 	const windowWidth = document.documentElement.clientWidth;
-	console.log(windowWidth);
-	if (windowWidth < 400 || windowWidth == 768) {
+
+	if (windowWidth < 400 || windowWidth === 768) {
 		setupIndicator("albums", 860);
 	} else {
 		setupFadeables(removeAndAdd("fade", "fade-out"), "albums", 2200);
+	}
+
+	if (windowWidth < 1024) {
+		onHomeInMobilePhones();
 	}
 };
 
@@ -96,6 +132,10 @@ const onAlbums = () => {
 };
 
 const onContacts = () => {
+	if (document.documentElement.clientWidth <= 1024) {
+		document.getElementById("contact-img").remove();
+	}
+
 	setupFadeables(
 		(delayed) => {
 			delayed.forEach((delay) => {
@@ -115,10 +155,7 @@ const onContacts = () => {
 
 switch (window.location.pathname) {
 	case "/":
-		setupFadeables(removeAndAdd("fade", "fade-out"), "albums", 2200);
-		break;
-	case "/mobile.html":
-		onMobile();
+		onHome();
 		break;
 	case "/albums.html": {
 		onAlbums();
